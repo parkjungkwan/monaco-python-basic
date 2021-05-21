e7a5e3afe0d7b44b0e61ee3635f57265d2e806e1
pakjkwan <pakjkwan@gmail.com>
2021-05-21 13:46:24 +0900

    13

--- list/account.py
+++ list/account.py
@@ -1,17 +1,32 @@
 class Account(object):
-    def __init__(self, b, c, d):
-        self.bank_name = 'sc 은행'
-        self.b = b
-        self.c = c
-        self.d = d
 
-    def get_account(self):
-        return f'{self.a}{self.b}{self.c}{self.d}'
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        self.BANK_NAME = 'SC은행'
+        self.acc_number = self.create_acc_number()
 
+    def create_acc_number(self):
+        pass
 
     @staticmethod
     def main():
+        ls = []
         while 1:
-            menu = input('')
-            if menu == 0:
+            menu = input('0.Exit 1.Create 2.Read 3.Update 4.Delete')
+            if menu == '0':
                 break
+            elif menu == '1':
+                pass
+            elif menu == '2':
+                pass
+            elif menu == '3':
+                pass
+            elif menu == '4':
+                pass
+            else :
+                print('Wrong Number')
+                continue
+
+Stock.main()
+
